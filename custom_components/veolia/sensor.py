b93c54f807c8a4996479407cd50d1bffc59370bf
Thibaut Etienne <thibaut@etienne.pw>
2020-12-12 00:59:45 +0100

    Add monthly report

--- custom_components/veolia/sensor.py
+++ custom_components/veolia/sensor.py
@@ -8,7 +8,12 @@ from .entity import VeoliaEntity
 async def async_setup_entry(hass, entry, async_add_devices):
     """Setup sensor platform."""
     coordinator = hass.data[DOMAIN][COORDINATOR]
-    async_add_devices([VeoliaDailyUsageSensor(coordinator, entry)])
+    async_add_devices(
+        [
+            VeoliaDailyUsageSensor(coordinator, entry),
+            VeoliaMonthlyUsageSensor(coordinator, entry),
+        ]
+    )
 
 
 class VeoliaDailyUsageSensor(VeoliaEntity):
@@ -23,6 +28,30 @@ class VeoliaDailyUsageSensor(VeoliaEntity):
     def state(self):
         """Return the state of the sensor."""
         return list(self.coordinator.data.values())[-1]
+
+    @property
+    def icon(self) -> str:
+        """Return the daily usage icon."""
+        return "mdi:water"
+
+    @property
+    def unit_of_measurement(self) -> str:
+        """Return liter as the unit measurement for water."""
+        return VOLUME_LITERS
+
+
+class VeoliaMonthlyUsageSensor(VeoliaEntity):
+    """Monitors the daily water usage."""
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return "veolia_monthly_consumption"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return sum(self.coordinator.data.values())
 
     @property
     def icon(self) -> str:
