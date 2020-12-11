91cca2edec225416799e3e0375c0b53e5dc1c311
Thibaut <thibaut@etienne.pw>
2020-12-11 16:52:41 +0100

    Update poll interval (#4)

--- custom_components/veolia/__init__.py
+++ custom_components/veolia/__init__.py
@@ -16,7 +16,7 @@ from pyolia.client import VeoliaClient
 
 from .const import CONF_PASSWORD, CONF_USERNAME, DOMAIN, PLATFORMS, STARTUP_MESSAGE
 
-SCAN_INTERVAL = timedelta(seconds=30)
+SCAN_INTERVAL = timedelta(days==1)
 
 _LOGGER = logging.getLogger(__name__)
 
